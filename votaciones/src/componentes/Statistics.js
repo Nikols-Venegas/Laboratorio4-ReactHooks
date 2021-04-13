0ab2502e7876cd47ad140b8e8095ec693f213b5f
unknown <nikols.venegas@tecsup.edu.pe>
2021-04-12 22:49:21 -0500

    4. conditional render - VOTACIONES

--- votaciones/src/componentes/Statistics.js
+++ votaciones/src/componentes/Statistics.js
@@ -1,14 +1,20 @@
-export default function Statics(props){
-    const{}=props;
-    return(
-        <div>
-            <h2>statistics</h2>
-            <p>good {props.good}</p>
-            <p>neutral {props.neutral}</p>
-            <p>bad {props.bad}</p>
-            <p>all {props.good+props.neutral+props.bad}</p>
-            <p>average {props.average/(props.good+props.neutral+props.bad)}</p>
-            <p>positive {(props.good/(props.good+props.neutral+props.bad))*100}%</p>
-        </div>
-    )
+export default function Statics(props) {
+	const {} = props;
+	return (
+		<div>
+			<h2>statistics</h2>
+			{props.good + props.bad + props.neutral == 0 ? (
+				<p>No feedback given</p>
+			) : (
+				<div>
+					<p>good {props.good}</p>
+					<p>neutral {props.neutral}</p>
+					<p>bad {props.bad}</p>
+					<p>all {props.good + props.neutral + props.bad}</p>
+					<p>average {props.average / (props.good + props.neutral + props.bad)}</p>
+					<p>positive {props.good / (props.good + props.neutral + props.bad) * 100}%</p>
+				</div>
+			)}
+		</div>
+	);
 }
--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -1,46 +1,43 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import Statics from './componentes/Statistics.js'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import Statics from './componentes/Statistics';
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
-  const [average, setAverage] = useState(0)
+	// save clicks of each button to its own state
+	const [ good, setGood ] = useState(0);
+	const [ neutral, setNeutral ] = useState(0);
+	const [ bad, setBad ] = useState(0);
 
+	const [ average, setAverage ] = useState(0);
 
-  const clicgood = () => {
-    setGood(good+1)
-    setAverage(average+1)
-  }
-  
-  const clicneutral = () => {
-    setNeutral(neutral+1)
-    setAverage(average+0)
-  }
+	const clicgood = () => {
+		setGood(good + 1);
+		setAverage(average + 1);
+	};
 
-  const clicbad = () => {
-    setBad(bad+1)
-    setAverage(average-1)
-  }
-  
+	const clicneutral = () => {
+		setNeutral(neutral + 1);
+		setAverage(average + 0);
+	};
 
+	const clicbad = () => {
+		setBad(bad + 1);
+		setAverage(average - 1);
+	};
 
-  return (
-    <div>
-      <div>
-        <h2>give feedback</h2>
-        <button onClick={clicgood}>good</button>
-        <button onClick={clicneutral}>neutral</button>
-        <button onClick={clicbad}>bad</button>
-      </div>
-      <div>
-        <Statics good={good} neutral={neutral} bad={bad} average={average}></Statics>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<div>
+				<h2>give feedback</h2>
+				<button onClick={clicgood}>good</button>
+				<button onClick={clicneutral}>neutral</button>
+				<button onClick={clicbad}>bad</button>
+			</div>
+			<div>
+				<Statics good={good} neutral={neutral} bad={bad} average={average} />
+			</div>
+		</div>
+	);
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
